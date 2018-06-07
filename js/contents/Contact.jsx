f126f8ed057b05dca0fbfa66751702c82dd7ec1d
coord.e <me@coord-e.com>
2018-06-08 00:24:25 +0900

    Enable validation on the form

--- js/contents/Contact.jsx
+++ js/contents/Contact.jsx
@@ -7,19 +7,30 @@ const styles = {
   form: require('css/form.css')
 }
 
+const validate = (e) => {
+  e.preventDefault()
+  const form = document.getElementById("contact-form")
+  if(form.reportValidity()) {
+    grecaptcha.execute()
+  }
+}
+
 export const Contact = () => (
   <div class={styles.style.container}>
     <h2 class={styles.style.heading}>Contact</h2>
     <div class={styles.form.container}>
       <form class={styles.form.form} id="contact-form" action="https://script.google.com/macros/s/AKfycbylioM8lYDDPxHI6HitD7ByH-tc7EXIC842VnyG4WUlbGGS3-A/exec" method="post">
         <label for="email" class={styles.form.label}>Email</label>
-        <input type="email" name="email" class={styles.form.email} />
+        <input type="email" name="email" class={styles.form.email} required />
         <label for="message" class={styles.form.label}>Message</label>
-        <textarea name="message" class={styles.form.message}></textarea>
-        <button
+        <textarea name="message" class={styles.form.message} required></textarea>
+        <div
           class={cx(styles.form.submit, "g-recaptcha")}
           data-sitekey="6LcFcEgUAAAAANjJW3BemaRnHeNDXQy6Pvz8ETgL"
-          data-callback="onSubmit">
+          data-callback="onSubmit"
+          data-size="invisible">
+        </div>
+        <button onclick={validate}>
           送信する
         </button>
       </form>
