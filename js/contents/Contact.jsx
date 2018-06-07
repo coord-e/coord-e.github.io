d81e2b170861e1398ebf8e0f1bbe508d00cac95f
coord.e <me@coord-e.com>
2018-06-07 23:38:29 +0900

    Implement contact form using GAS and reCAPTCHA

--- js/contents/Contact.jsx
+++ js/contents/Contact.jsx
@@ -1,3 +1,5 @@
+import cx from 'classnames'
+
 import { h, app } from 'hyperapp'
 
 const styles = {
@@ -9,12 +11,17 @@ export const Contact = () => (
   <div class={styles.style.container}>
     <h2 class={styles.style.heading}>Contact</h2>
     <div class={styles.form.container}>
-      <form class={styles.form.form}>
+      <form class={styles.form.form} id="contact-form" action="https://script.google.com/macros/s/AKfycbylioM8lYDDPxHI6HitD7ByH-tc7EXIC842VnyG4WUlbGGS3-A/exec" method="post">
         <label for="email" class={styles.form.label}>Email</label>
         <input type="email" name="email" class={styles.form.email} />
         <label for="message" class={styles.form.label}>Message</label>
         <textarea name="message" class={styles.form.message}></textarea>
-        <input type="submit" value="送信する" class={styles.form.submit} />
+        <button
+          class={cx(styles.form.submit, "g-recaptcha")}
+          data-sitekey="6LcFcEgUAAAAANjJW3BemaRnHeNDXQy6Pvz8ETgL"
+          data-callback="onSubmit">
+          送信する
+        </button>
       </form>
     </div>
   </div>
