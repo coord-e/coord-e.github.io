768615607d87f6ac4df567e09d51d909c20c2ea5
coord.e <me@coord-e.com>
2018-06-07 17:59:23 +0900

    Add Icon and IconLink component to show icon

---
+++ js/Icon.jsx
@@ -0,0 +1,46 @@
+import cx from "classnames"
+
+import { h } from 'hyperapp'
+
+const styles = {
+  style: require("css/style.css")
+}
+
+export const Icon = (props) => {
+  const {
+    src: {
+      icon: [
+        iconWidth,
+        iconHeight,
+        , ,
+        iconPath
+      ],
+      iconName,
+      prefix: iconPrefix
+    },
+    name = iconName,
+    viewBox = `0 0 ${iconWidth} ${iconHeight}`,
+    prefix = iconPrefix,
+    path = iconPath
+  } = props
+
+  const className = props.class
+
+  return <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    role="img"
+    data-icon={ name }
+    data-prefix={ prefix }
+    class={cx(styles.style.icon, className)}
+    viewBox={ viewBox }
+  >
+    <path fill="currentColor" d={path}></path>
+  </svg>
+}
+
+export const IconLink = (props) => (
+  <a href={props.href} target={props.target}>
+    <Icon {...props} />
+  </a>
+)
